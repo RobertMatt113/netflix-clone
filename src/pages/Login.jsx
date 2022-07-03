efbd5433a70cd9e71045dee0c286de6e703bf63e
RobertMatt113 <robertmatt113@gmail.com>
2022-07-03 16:26:37 -0500

    all done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,11 @@ const Login = () => {
 
     return (
         <div className='w-full h-screen'>
-                <img className='hidden sm:block absolute w-full h-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/271ac55e-7228-438e-824e-92db37981e59/31970505-d557-4c83-982b-57b4a27007a7/MX-es-20220627-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+                <img 
+                    className='hidden sm:block absolute w-full h-full object-cover' 
+                    src="https://assets.nflxext.com/ffe/siteui/vlv3/271ac55e-7228-438e-824e-92db37981e59/31970505-d557-4c83-982b-57b4a27007a7/MX-es-20220627-popsignuptwoweeks-perspective_alpha_website_medium.jpg" 
+                    alt="/" 
+                />
                 <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
                 <div className='fixed w-full px-4 py-24 z-50'>
                     <div className='max-w-[450px] h-[480px] mx-auto bg-black/75 text-white'>
